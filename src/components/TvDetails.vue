2c6d082fcadd69df5b00a628a9c10428dadad8ac
karthik547 <40254567+karthik547@users.noreply.github.com>
2022-05-17 18:06:51 +0530

    Update TvDetails.vue

--- src/components/TvDetails.vue
+++ src/components/TvDetails.vue
@@ -2,42 +2,9 @@
   <div>
     Details {{ userId }}
     <div>
-      <h2>Adventure</h2>
-      <span v-for="item in tvShows" :key="item.id">
-        <span v-for="diff in item.genres" :key="diff">
-          <span v-if="diff === 'Adventure' && item.id == userId">
-            <span class="card">
-              <div>{{ item.summary}}</div>
-              <img :src="item.image.medium" />
-              <div class="container">
-                <h4>
-                  <b>{{ item.name }}</b>
-                </h4>
-                <p>{{ item.rating.average }}</p>
-              </div>
-            </span>
-          </span>
-        </span>
-      </span>
-    </div>
-    <div>
-      <h2>Action</h2>
-      <span v-for="item in tvShows" :key="item.id">
-        <span v-for="diff in item.genres" :key="diff">
-          <span v-if="diff === 'Action' && item.id == userId">
-            <span class="card">
-              <div>{{ item.summary}}</div>
-              <img :src="item.image.medium" />
-              <div class="container">
-                <h4>
-                  <b>{{ item.name }}</b>
-                </h4>
-                <p>{{ item.rating.average }}</p>
-              </div>
-            </span>
-          </span>
-        </span>
-      </span>
+        <h2>{{name}}</h2>
+        <img :src="tvShows1" />
+        <p>{{details}}</p>
     </div>
   </div>
 </template>
@@ -45,24 +12,14 @@
 <script>
 export default {
   name: "TvDetails",
-  props: {
-    name: String,
-    showItem: String,
-  },
+  props: ["tvShows"],
   data() {
     return {
-      details: "",
+      details: this.$route.params.details,
       userId: this.$route.params.id,
-      tvShows: [],
+      tvShows1: this.$route.params.obj,
+      name: this.$route.params.name
     };
-  },
-  created: function () {
-    fetch("https://api.tvmaze.com/shows")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data, "data..");
-        this.tvShows = data;
-      });
   },
 };
 </script>
