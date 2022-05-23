9126aae41f59757b464f6e8fe83f8b7001fae83b
karthik547 <40254567+karthik547@users.noreply.github.com>
2022-05-23 12:20:21 +0530

    Update tvdetails.spec.js

--- tests/unit/tvdetails.spec.js
+++ tests/unit/tvdetails.spec.js
@@ -1,10 +1,23 @@
-import { mount } from "@vue/test-utils";
-import tvDetails from '@/components/tvDetails';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import tvDetails from "@/components/tvDetails";
 
-describe('Tv Details',()=>{
-    it('details should be render',()=>{
-        const wrapper = mount(tvDetails);
-
-        expect(wrapper.text()).toContain();
-    })
-})
+describe("tvDetails.vue", () => {
+  it("should totest renders stuff", (done) => {
+    Vue.use(VueRouter);
+    const router = new VueRouter({
+      routes: [{ path: "/shows/:id", name: "tvDetails", component: tvDetails }],
+    });
+    const vm = new Vue({
+      el: document.createElement("div"),
+      router: router,
+      render: (h) => h("router-view"),
+    });
+    router.push({ name: "tvDetails", params: { id: 204 } });
+    Vue.nextTick(() => {
+      console.log("html:", vm.$el);
+      expect(vm.$el.querySelector("h2").textContent).to.equal("Stargate SG-1");
+      done();
+    });
+  });
+});
